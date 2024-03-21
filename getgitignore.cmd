46ea7ba8c7dd8887d90153201b58e733e9abe331
jaka2005 <jakalogin1808@gmail.com>
2024-03-22 01:22:29 +0300

    added out param for getgitignore

--- getgitignore.cmd
+++ getgitignore.cmd
@@ -1,3 +1,9 @@
 @echo off
 
-call getgh -a github -r gitignore -f %1.gitignore -o .gitignore
+set out=.gitignore
+
+if "%2" NEQ "" (
+    set out=%2
+)
+
+call getgh -a github -r gitignore -f %1.gitignore -o %2
